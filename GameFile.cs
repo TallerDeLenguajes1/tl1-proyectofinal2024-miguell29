15946a8df6fcc4ec38e82347e7f13ee07bde3462
Miguel Juarez <mfjuaarez@gmail.com>
2024-07-18 14:09:02 -0300

    se agrego los metodos para leer y escribir el historial de los ganadores en la clase GameFile

--- GameFile.cs
+++ GameFile.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.Text.Json;
 using ConsoleGame;
 
@@ -5,11 +6,17 @@ namespace GameComplements;
 
 public static class GameFile
 {
+    //*Campo que almacena el path de la carpeta donde se guardaran los archivos
+    //se crea un campo para evitar la repeticion de codigo
+    // y prop para cambiarlo en un solo lugar en caso de ser necesario
+    private static string carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files"); 
+    public static string Carpeta { get; set; }
+    
     public static void GuardarPersonajes(List<Personaje> lista)
     {
         var DataJson = JsonSerializer.Serialize(lista);
         var fileName = "Personajes.json";
-        var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files"); //* Crea el path de la carpeta FILES que contiene los archivos generados
+        //var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files"); //* Crea el path de la carpeta FILES que contiene los archivos generados
         if (!Directory.Exists(carpeta))
         {
             Directory.CreateDirectory(carpeta);
@@ -18,14 +25,40 @@ public static class GameFile
     }
     public static List<Personaje> LeerPersonajes(string fileName)
     {
-        var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files");
+       // var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files");
         var dataJson = File.ReadAllText(Path.Combine(carpeta,fileName));
         List<Personaje> personajes = JsonSerializer.Deserialize<List<Personaje>>(dataJson);
         return personajes;
     }
+    public static void GuardarGanador(Personaje ganador)
+    {
+        var datajson = JsonSerializer.Serialize(ganador);
+        var datos = new List<string>
+        {
+            datajson
+        };
+        var fileName = "HistorialGanadores.json";
+       // var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files");
+        if (!Directory.Exists(carpeta)){
+            Directory.CreateDirectory(carpeta);
+        }
+        File.AppendAllLines(Path.Combine(carpeta,fileName),datos);
+    }
+    public static List<Personaje> LeerGanadores(string fileName)
+    {
+        //var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files");
+        var listaGanadoresJson = File.ReadAllLines(Path.Combine(carpeta,fileName));//lista de strings con los ganadores en formato json
+        var listaGanadores = new List<Personaje>();
+        foreach (var ganadorJson in listaGanadoresJson)
+        {
+            var ganador = JsonSerializer.Deserialize<Personaje>(ganadorJson);
+            listaGanadores.Add(ganador);
+        }
+        return listaGanadores;
+    }
     public static bool Existe(string fileName)
     {
-        var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files");
+        //var carpeta = Path.Combine(Directory.GetCurrentDirectory(),"Files");
         if (!Directory.Exists(carpeta))
         {
             Directory.CreateDirectory(carpeta);
